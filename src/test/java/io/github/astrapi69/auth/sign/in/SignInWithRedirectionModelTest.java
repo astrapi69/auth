a6a07079816f345dcab77a401fb4d9ca17f2fe57
astrapi69 <asterios.raptis@gmx.net>
2022-02-28 14:18:46 +0100

    formatted and organized imports

--- src/test/java/io/github/astrapi69/auth/sign/in/SignInWithRedirectionModelTest.java
+++ src/test/java/io/github/astrapi69/auth/sign/in/SignInWithRedirectionModelTest.java
@@ -28,8 +28,6 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Optional;
 
 import org.meanbean.test.BeanTester;
