3bd5f34a98190ca12f530cff5cd01bd3a104b684
Asterios Raptis <asteri.raptis@gmail.com>
2024-07-19 14:49:29 +0200

    formatted with spotless

--- src/main/java/io/github/astrapi69/auth/sign/up/package-info.java
+++ src/main/java/io/github/astrapi69/auth/sign/up/package-info.java
@@ -1,4 +1,5 @@
 /**
- * This package provides classes for the sign up process in the area of authentication and authorization
+ * This package provides classes for the sign up process in the area of authentication and
+ * authorization
  */
 package io.github.astrapi69.auth.sign.up;
